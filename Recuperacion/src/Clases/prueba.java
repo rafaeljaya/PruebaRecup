3f9bfb6bcd62a153fece6a2beae5749c89fc2a94
Rafael Jaya <Rafael Jaya@172.17.161.184>
2026-06-05 12:08:20 -0500

    Commit2

--- Recuperacion/src/Clases/prueba.java
+++ Recuperacion/src/Clases/prueba.java
@@ -5,6 +5,7 @@ public class prueba {
 	public static void main(String[] args) {
         System.out.println("prueba de git");
         System.out.println("Actualizacion");
+        System.out.println("Actualizacion 2");
 
     }
 
